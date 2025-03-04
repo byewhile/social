43a748598dec0e6757fb4e05f429f4215e30c78c
byewhile <93428940+byewhile@users.noreply.github.com>
2025-03-04 19:38:03 +0300

    test

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,12 +1,35 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { useParams } from "next/navigation";
+import axios from "axios"
 
 export default function page() {
+    const [data, setData] = useState([]);
     const { id } = useParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("https://byewhile.ru/db.php")
+                setData(res.data)
+            } catch(err) {
+                console.log(err)
+            }
+            return true;
+        }
+        fetchData()
+    }, [])
 
     return (
         <div>
+            {data.map(({index, data}: any) => {
+                {console.log(data)}
+                return <div key={index}>
+                    <p>{data.id}</p>
+                    <p>{data.name}</p>
+                </div>
+            })}
             {id}
         </div>
     )
